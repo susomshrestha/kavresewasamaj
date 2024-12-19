62e6c557ba3c4da37dff583c442ad48a4a9aa115
Susom <susomshrestha@gmail.com>
2024-12-19 14:14:07 -0500

    add design for footer

--- front-kavresewasamaj/src/App.tsx
+++ front-kavresewasamaj/src/App.tsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './pages/Home/home';
+import Footer from './components/footer';
 
 function App() {
 	return (
 		<>
-			<Navbar />
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<Routes>
+					<Route index element={<Home />} />
+					{/* <Route path="contact" element={<Contact />} /> */}
+				</Routes>
+			</main>
+			<Footer />
 		</>
 	);
 }
---
+++ front-kavresewasamaj/src/components/footer.tsx
@@ -0,0 +1,101 @@
+import { NavLink } from 'react-router';
+import logo from '../assets/logo.png';
+
+const Footer = () => {
+	return (
+		<footer className="bg-white">
+			<div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+				<div className="md:flex md:justify-evenly">
+					<div className="mb-6 md:mb-0 flex items-center justify-evenly">
+						<NavLink to="/" className="flex">
+							<img src={logo} className="h-16 me-3" alt="Kavre Sewa Samaj Logo" />
+							<span className="self-center text-2xl font-semibold whitespace-nowrap">
+								Kavre Sewa Samaj
+							</span>
+						</NavLink>
+					</div>
+
+					<div className="grid grid-cols-2 gap-8 sm:gap-6 px-2">
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">About</h2>
+							<ul className="text-gray-500 font-medium">
+								<li className="mb-4">
+									<NavLink to="/" className="hover:underline">
+										Events
+									</NavLink>
+								</li>
+								<li>
+									<NavLink to="/" className="hover:underline">
+										Contact Us
+									</NavLink>
+								</li>
+							</ul>
+						</div>
+
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+							<ul className="text-gray-500 font-medium">
+								<li className="mb-4">
+									<a href="#" className="hover:underline">
+										Privacy Policy
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Terms &amp; Conditions
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
+				<div className="sm:flex sm:items-center sm:justify-between">
+					<span className="text-sm text-gray-500 sm:text-center">
+						© 2024 . All Rights Reserved.
+					</span>
+
+					<div className="flex mt-4 sm:justify-center sm:mt-0">
+						<a
+							href="https://www.facebook.com/KavreSewaSamaj/"
+							className="text-gray-500 hover:text-gray-900">
+							<svg
+								className="w-4 h-4"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 8 19">
+								<path
+									fillRule="evenodd"
+									d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							<span className="sr-only">Facebook page</span>
+						</a>
+
+						<a href="https://x.com/samajkavre" className="text-gray-500 hover:text-gray-900 ms-5">
+							<svg
+								className="w-4 h-4"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 17">
+								<path
+									fillRule="evenodd"
+									d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							<span className="sr-only">Twitter page</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
